08f0f331550f829a9c638aa90a1cd7e75e51b499
Jawnathan <jonathan@jonathanschimpf.com>
2024-03-26 15:28:43 -0400

    -m

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,27 @@
 <script>
-	import 'bootstrap/dist/css/bootstrap.min.css';
+	// NAV COMPONENT IMPORT
 	import Nav from '$lib/Nav.svelte';
-</script>
-
-<Nav />
-<slot />
-
-<style>
-    	:global(body, html) {
-		background-color: rgb(255, 255, 255) !important; /* SPECIFIC OVERRIDE */
-		font-family: 'Futura', sans-serif; /* Futura OBVIOUSLY */
+  
+	// BOOTSTRAP CSS IMPORT
+	import 'bootstrap/dist/css/bootstrap.min.css';
+  
+	// VITE'S WAY OF HANDLING STATIC ASSETS
+	import bootstrapBundle from 'bootstrap/dist/js/bootstrap.bundle.min.js?url';
+  </script>
+  
+  <svelte:head>
+	<!-- BOOTSTRAP'S JS BUNDLE SO IT WILL BE SERVED SERVER SIDE -->
+	<script src="{bootstrapBundle}" defer></script>
+  </svelte:head>
+  
+  <!-- NAV AND SLOT FOR PAGE CONTENT -->
+  <Nav />
+  <slot />
+  
+  <style>
+	:global(body, html) {
+	  background-color: rgb(255, 255, 255) !important; /* SPECIFIC OVERRIDE */
+	  font-family: 'Futura', sans-serif; 
 	}
-</style>
+  </style>
+  
