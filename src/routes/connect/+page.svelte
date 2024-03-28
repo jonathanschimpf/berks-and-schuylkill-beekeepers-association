a914096f45ed341f941ca30d2137e90643594620
Jawnathan <jonathan@jonathanschimpf.com>
2024-03-27 21:30:08 -0400

    🐝

--- src/routes/connect/+page.svelte
+++ src/routes/connect/+page.svelte
@@ -1,160 +1,115 @@
 <script>
+	import { onMount } from 'svelte';
 	import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
 	import { faFacebook } from '@fortawesome/free-brands-svg-icons';
-	import { onMount } from 'svelte';
 	import { fade } from 'svelte/transition';
-
-	let fbLoaded = false;
-
-	onMount(() => {
-		let fbRoot = document.getElementById('fb-root');
-		if (!fbRoot) {
-			fbRoot = document.createElement('div');
-			fbRoot.id = 'fb-root';
-			document.body.insertBefore(fbRoot, document.body.firstChild);
-		}
-
-		const script = document.createElement('script');
-		script.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v19.0';
-		script.async = true;
-		script.defer = true;
-		script.crossOrigin = 'anonymous';
-		script.onload = () => {
-			if (window.FB) {
-				FB.init({
-					xfbml: true,
-					version: 'v19.0'
-				});
-				FB.XFBML.parse();
-			}
-			fbLoaded = true;
-		};
-		document.body.appendChild(script);
-	});
-</script>
-
-<div class="topcenteringcontainer" in:fade={{ duration: 2000 }}>
-	<div class="heading-container">
+  </script>
+  
+  <div class="all-connect-items-centered">
+	<div class="topcenteringcontainer">
+	  <div class="heading-container">
 		<a
-			href="https://www.facebook.com/BerksSchuylkillBeekeepers"
-			rel="noreferrer"
-			target="_blank"
-			aria-label="Berks Schuylkill Beekeepers Association"
-			class="no-text-decoration heading-link"
+		  href="https://www.facebook.com/BerksSchuylkillBeekeepers"
+		  rel="noreferrer"
+		  target="_blank"
+		  aria-label="Berks Schuylkill Beekeepers Association"
+		  class="heading-link"
 		>
-			<h2>Connect</h2>
+		  <h2 class="no-underline">Connect</h2>
 		</a>
 		<a
-			href="https://www.facebook.com/BerksSchuylkillBeekeepers"
-			rel="noreferrer"
-			target="_blank"
-			aria-label="Berks Schuylkill Beekeepers Association"
-			class="icon-link"
+		  href="https://www.facebook.com/BerksSchuylkillBeekeepers"
+		  rel="noreferrer"
+		  target="_blank"
+		  aria-label="Facebook Page"
+		  class="icon-link"
 		>
-			<FontAwesomeIcon icon={faFacebook} class="fa-2xl" style="color: #4267B2;" />
+		  <FontAwesomeIcon icon={faFacebook} class="fa-2xl" style="color: #4267B2; margin-bottom: 10px;" />
 		</a>
-	</div>
-
-	<a
+	  </div>
+	  <a
 		href="https://www.facebook.com/BerksSchuylkillBeekeepers"
 		rel="noreferrer"
 		target="_blank"
 		aria-label="Beekeepers Connecting"
-	>
+	  >
 		<img
-			src={`/images/douglas-raggio-vZGvVTO1pIg-unsplash.jpg`}
-			class="beekeepersconnectingphoto"
-			alt="Beekeepers Connecting"
+		  src={`/images/douglas-raggio-vZGvVTO1pIg-unsplash.jpg`}
+		  class="beekeepersconnectingphoto"
+		  alt="Beekeepers Connecting"
 		/>
-	</a>
-</div>
-
-{#if !fbLoaded}
-	<div class="loading-container" in:fade={{ duration: 2500 }}>
-		<p class="loading-facebook-msg">Loading Facebook...</p>
+	  </a>
 	</div>
-{:else}
-	<!-- Facebook PLUG-IN -->
+  
 	<div class="facebook-plugin-container" in:fade={{ duration: 3000 }}>
-		<div
-			class="fb-page"
-			data-href="https://www.facebook.com/BerksSchuylkillBeekeepers"
-			data-tabs="timeline"
-			data-small-header="false"
-			data-adapt-container-width="true"
-			data-hide-cover="false"
-			data-show-facepile="true"
-			data-height="5000"
-		></div>
+	  <iframe
+		src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FBerksSchuylkillBeekeepers&tabs=timeline&width=350&height=2500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+		width="350"
+		height="2500"
+		style="border:none;overflow:hidden"
+		scrolling="no"
+		frameborder="0"
+		allowfullscreen="true"
+		allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+		title="Facebook Social Plugin"
+	  ></iframe>
 	</div>
-{/if}
-
-<style>
+  </div>
+  
+  <style>
+	.all-connect-items-centered {
+	  display: flex;
+	  flex-direction: column;
+	  align-items: center;
+	  justify-content: center;
+	  margin-top: 80px;
+	}
+  
 	.heading-container {
-		display: flex;
-		align-items: center;
+	  display: flex;
+	  align-items: center;
 	}
-
+  
 	.heading-link {
-		display: flex;
-		align-items: center;
+	  display: flex;
+	  align-items: center;
+	  text-decoration: none;
 	}
-
-	.heading-link h2 {
-		margin-right: 0.5rem;
+  
+	.no-underline {
+	  text-decoration: none;
+	  margin-right: 5.5rem;
+	  color: black;
 	}
-
+  
 	.icon-link {
-		display: flex;
-		align-items: center;
-		line-height: 1;
-		margin-left: 7rem;
-		padding-bottom: 0.80rem;
+	  display: flex;
+	  align-items: center;
+	  line-height: 1;
+	  margin-left: 1rem;
 	}
-
+  
 	.topcenteringcontainer {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		margin-top: 8.5em;
+	  display: flex;
+	  flex-direction: column;
+	  align-items: center;
+	  justify-content: center;
+	  margin-top: 2em;
 	}
-
+  
 	.beekeepersconnectingphoto {
-		width: 343px;
-		border-radius: 5px;
-		display: block;
+	  width: 100%;
+	  max-width: 350px;
+	  border-radius: 5px;
 	}
-	.topcenteringcontainer a {
-		text-decoration: none;
-		color: inherit;
-	}
-
-	.loading-container {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		min-height: 10vh;
-	}
-
-	.loading-facebook-msg {
-		color: #4267b2;
-	}
+  
 	.facebook-plugin-container {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		max-width: 95vw;
-		max-height: 95vh;
-		margin: 0 auto;
-		padding: 1em;
+	  display: flex;
+	  justify-content: center;
+	  align-items: center;
+	  width: 100%;
+	  max-width: 350px;
+	  margin-top: 1em;
 	}
-	.fb-page {
-		height: 80vh;
-		border-radius: 5px;
-	}
-	h2 {
-		color: rgb(0, 0, 0);
-		font-weight: 900;
-	}
-</style>
+  </style>
+  
